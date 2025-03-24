34d189dd9c55811eb7645cda8349109b49426135
Ben Brick <113672740+BenBrick@users.noreply.github.com>
2025-03-24 17:52:31 +0000

    Scenario 1

--- VAR Project/VAR FYP/Assets/Scripts/BallCorner.cs
+++ VAR Project/VAR FYP/Assets/Scripts/BallCorner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BallCorner : MonoBehaviour
 {
@@ -9,18 +10,29 @@ public class BallCorner : MonoBehaviour
     private float speed = 2.00f;
     private float minSpeed = 0.2f;
     private float slow = 0.99f;
+    GameObject[] goalLocations;
     private Rigidbody rb;
+    NavMeshAgent agent;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        agent = this.GetComponent<NavMeshAgent>();
+        goalLocations = GameObject.FindGameObjectsWithTag("HeaderPlayer");
+    }
+    
+    public void StartAgent()
+    {
+        agent.SetDestination(goalLocations[0].transform.position);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            agent.isStopped = true;
+            agent.enabled = false;
             Rigidbody playerRB = collision.gameObject.GetComponent<Rigidbody>();
             
             if (playerRB != null)
@@ -30,17 +42,19 @@ public class BallCorner : MonoBehaviour
                 Vector3 momentum = Vector3.Project(rb.velocity * rb.mass, dir);
                 rb.velocity -= momentum;
                 
-                Vector3 forward = dir * (force * 6f);
+                Vector3 forward = dir * (force * 7f);
                 Vector3 right = transform.forward * (force * 9.4f);
-                Vector3 up = transform.up * (force * 1.8259f);
+                Vector3 up = transform.up * (force * 3.9f);
 
                 rb.AddForce(forward - right + up, ForceMode.Impulse);
-                
+                StartCoroutine(navMeshAgent());
             }
         }
 
         if (collision.gameObject.CompareTag("HeaderPlayer"))
         {
+            agent.isStopped = true;
+            agent.enabled = false;
             Rigidbody playerRB2 = collision.gameObject.GetComponent<Rigidbody>();
 
             if (playerRB2 != null)
@@ -49,12 +63,21 @@ public class BallCorner : MonoBehaviour
                 Vector3 momentum = Vector3.Project(rb.velocity * rb.mass, dir);
                 rb.velocity -= momentum;
                 
-                Vector3 forward = dir * (force * 4f);
+                //Vector3 forward = dir * (force * 4f);
                 Vector3 right = transform.forward * (force * 10f);
                 Vector3 down = transform.up * (force * 1.84f);
-                rb.AddForce(forward - right - down, ForceMode.Impulse);
+                rb.AddForce(right - down, ForceMode.Impulse);
             }
         }
+    }
+
+    private IEnumerator navMeshAgent()
+    {
+        yield return new WaitForSeconds(0.63f);
+        agent.enabled = true;
+        agent.isStopped = false;
+        rb.velocity = Vector3.zero;
+        StartAgent();
     }
 
     // Update is called once per frame
